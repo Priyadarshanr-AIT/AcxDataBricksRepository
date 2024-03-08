15c15656342d8767420d4e2f2e1afa7856a55509
Priyadarshanr-AIT <Priyadarshanr-AIT>
2024-03-08 07:53:04 +0000

    Bronze to Silver Container
    Data transformation for the DateModified Field and transferring the tables from bronze to silver container

--- bronze to silver.py
+++ bronze to silver.py
@@ -33,4 +33,25 @@ table_name
 
 # COMMAND ----------
 
+from pyspark.sql.functions import from_utc_timestamp, date_format
+from pyspark.sql.types import TimestampType
+
+for i in table_name:
+    path = '/mnt/bronze/Sales/' + i + '/' + i + '.parquet'
+    df = spark.read.format('parquet').load(path)
+    column = df.columns
+
+    for col in column:
+        if "Date" in col or "date" in col:
+            df = df.withColumn(col, date_format(from_utc_timestamp(df[col].cast(TimestampType()), "UTC"), "yyyy-MM-dd"))
+
+    output_path = '/mnt/silver/Sales/' + i + '/'
+    df.write.format('delta').mode("overwrite").save(output_path)
+
+# COMMAND ----------
+
+display(df)
+
+# COMMAND ----------
+
 
